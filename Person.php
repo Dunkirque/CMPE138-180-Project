ee0c7ce9a1859ead0305eb87e0291fe67b2f0833
Dunkirque <andrewpayoyo@gmail.com>
2023-11-15 13:31:39 -0800

    Update Person.php

--- Person.php
+++ Person.php
@@ -3,7 +3,7 @@
 require_once('db_connect.php');
 
 // Initialize variables
-$PersonLname = $PersonFname = $PersonSSN = $PersonDOB = $UserRole = '';
+$PersonLname = $PersonFname = $PersonSSN = $PersonDOB = '';
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -12,17 +12,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $PersonFname = mysqli_real_escape_string($mysqli, $_POST["PersonFname"]);
     $PersonSSN = mysqli_real_escape_string($mysqli, $_POST["PersonSSN"]);
     $PersonDOB = mysqli_real_escape_string($mysqli, $_POST["PersonDOB"]);
-    $UserRole = mysqli_real_escape_string($mysqli, $_POST["UserRole"]);
 
     // Insert data into the database
-    $query = "INSERT INTO Person (PersonLname, PersonFname, PersonSSN, PersonDOB, UserRole) 
-              VALUES ('$PersonLname', '$PersonFname', '$PersonSSN', '$PersonDOB', '$UserRole')";
+    $query = "INSERT INTO Person (PersonLname, PersonFname, PersonSSN, PersonDOB) 
+              VALUES ('$PersonLname', '$PersonFname', '$PersonSSN', '$PersonDOB')";
 
     if ($mysqli->query($query) === TRUE) {
         echo "Record inserted successfully";
 
         // Clear form fields after successful insertion
-        $PersonLname = $PersonFname = $PersonSSN = $PersonDOB = $UserRole = '';
+        $PersonLname = $PersonFname = $PersonSSN = $PersonDOB = '';
     } else {
         echo "Error: " . $query . "<br>" . $mysqli->error;
     }
@@ -34,7 +33,6 @@ $rows = $result->fetch_all(MYSQLI_ASSOC);
 
 // Close the database connection
 $mysqli->close();
-
 ?>
 
 <!DOCTYPE html>
@@ -62,36 +60,43 @@ $mysqli->close();
         <label for="PersonDOB">Date of Birth:</label>
         <input type="date" name="PersonDOB" value="<?php echo $PersonDOB; ?>" required><br>
 
+        <!-- Add a dropdown for user role selection -->
         <label for="UserRole">Select Role:</label>
         <select name="UserRole">
             <option value="LicenseRequestor">License Requestor</option>
             <option value="CurrentDriver">Current Driver</option>
             <option value="VehicleRegistrationRequestor">Vehicle Registration Requestor</option>
-        </select><br>
+        </select>
 
         <button type="submit">Submit</button>
     </form>
+
+    <?php
+    // Show button based on selected role after form submission
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["UserRole"])) {
+        $selectedRole = $_POST["UserRole"];
+
+        if ($selectedRole === 'LicenseRequestor') {
+            echo '<a href="LicenseRequestor.php"><button type="button">Go to License Requestor</button></a>';
+        } elseif ($selectedRole === 'CurrentDriver') {
+            echo '<a href="CurrentDriver.php"><button type="button">Go to Current Driver</button></a>';
+        } elseif ($selectedRole === 'VehicleRegistrationRequestor') {
+            echo '<a href="VehicleRegistrationRequestor.php"><button type="button">Go to Vehicle Registration Requestor</button></a>';
+        }
+    }
+    ?>
 
     <h2>Person Data List</h2>
 
     <ul>
         <?php if (isset($rows) && is_array($rows)): ?>
             <?php foreach ($rows as $row): ?>
-                <li><?php echo "{$row['PersonFname']} {$row['PersonLname']} - SSN: {$row['PersonSSN']}, DOB: {$row['PersonDOB']}, Role: {$row['UserRole']}"; ?></li>
+                <li><?php echo "{$row['PersonFname']} {$row['PersonLname']} - SSN: {$row['PersonSSN']}, DOB: {$row['PersonDOB']}"; ?></li>
             <?php endforeach; ?>
         <?php endif; ?>
     </ul>
 
-    <?php
-    // Add buttons to redirect based on the user's role
-    if ($UserRole == 'LicenseRequestor') {
-        echo '<a href="LicenseRequestor.php"><button type="button">Go to License Requestor</button></a>';
-    } elseif ($UserRole == 'CurrentDriver') {
-        echo '<a href="CurrentDriver.php"><button type="button">Go to Current Driver</button></a>';
-    } elseif ($UserRole == 'VehicleRegistrationRequestor') {
-        echo '<a href="VehicleRegistrationRequestor.php"><button type="button">Go to Vehicle Registration Requestor</button></a>';
-    }
-    ?>
+    <!-- No need for buttons related to specific roles here -->
 
     <a href="DrivingSchool.php">
         <button type="button">Go to Driving School Data</button>
