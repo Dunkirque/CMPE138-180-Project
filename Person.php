273165376b705c226cb35c87c2c5c1976f4379e1
Dunkirque <andrewpayoyo@gmail.com>
2023-11-15 12:26:27 -0800

    Update Person.php

--- Person.php
+++ Person.php
@@ -1,10 +1,9 @@
 <?php
-
 // Include the database connection file
 require_once('db_connect.php');
 
 // Initialize variables
-$PersonLname = $PersonFname = $PersonSSN = $PersonDOB = '';
+$PersonLname = $PersonFname = $PersonSSN = $PersonDOB = $UserRole = '';
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,16 +12,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $PersonFname = mysqli_real_escape_string($mysqli, $_POST["PersonFname"]);
     $PersonSSN = mysqli_real_escape_string($mysqli, $_POST["PersonSSN"]);
     $PersonDOB = mysqli_real_escape_string($mysqli, $_POST["PersonDOB"]);
+    $UserRole = mysqli_real_escape_string($mysqli, $_POST["UserRole"]);
 
     // Insert data into the database
-    $query = "INSERT INTO Person (PersonLname, PersonFname, PersonSSN, PersonDOB) 
-              VALUES ('$PersonLname', '$PersonFname', '$PersonSSN', '$PersonDOB')";
+    $query = "INSERT INTO Person (PersonLname, PersonFname, PersonSSN, PersonDOB, UserRole) 
+              VALUES ('$PersonLname', '$PersonFname', '$PersonSSN', '$PersonDOB', '$UserRole')";
 
     if ($mysqli->query($query) === TRUE) {
         echo "Record inserted successfully";
 
         // Clear form fields after successful insertion
-        $PersonLname = $PersonFname = $PersonSSN = $PersonDOB = '';
+        $PersonLname = $PersonFname = $PersonSSN = $PersonDOB = $UserRole = '';
     } else {
         echo "Error: " . $query . "<br>" . $mysqli->error;
     }
@@ -62,6 +62,13 @@ $mysqli->close();
         <label for="PersonDOB">Date of Birth:</label>
         <input type="date" name="PersonDOB" value="<?php echo $PersonDOB; ?>" required><br>
 
+        <label for="UserRole">Select Role:</label>
+        <select name="UserRole">
+            <option value="LicenseRequestor">License Requestor</option>
+            <option value="CurrentDriver">Current Driver</option>
+            <option value="VehicleRegistrationRequestor">Vehicle Registration Requestor</option>
+        </select><br>
+
         <button type="submit">Submit</button>
     </form>
 
@@ -70,10 +77,21 @@ $mysqli->close();
     <ul>
         <?php if (isset($rows) && is_array($rows)): ?>
             <?php foreach ($rows as $row): ?>
-                <li><?php echo "{$row['PersonFname']} {$row['PersonLname']} - SSN: {$row['PersonSSN']}, DOB: {$row['PersonDOB']}"; ?></li>
+                <li><?php echo "{$row['PersonFname']} {$row['PersonLname']} - SSN: {$row['PersonSSN']}, DOB: {$row['PersonDOB']}, Role: {$row['UserRole']}"; ?></li>
             <?php endforeach; ?>
         <?php endif; ?>
     </ul>
+
+    <?php
+    // Add buttons to redirect based on the user's role
+    if ($UserRole == 'LicenseRequestor') {
+        echo '<a href="LicenseRequestor.php"><button type="button">Go to License Requestor</button></a>';
+    } elseif ($UserRole == 'CurrentDriver') {
+        echo '<a href="CurrentDriver.php"><button type="button">Go to Current Driver</button></a>';
+    } elseif ($UserRole == 'VehicleRegistrationRequestor') {
+        echo '<a href="VehicleRegistrationRequestor.php"><button type="button">Go to Vehicle Registration Requestor</button></a>';
+    }
+    ?>
 
     <a href="DrivingSchool.php">
         <button type="button">Go to Driving School Data</button>
