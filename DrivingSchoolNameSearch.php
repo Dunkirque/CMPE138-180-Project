b6341e8ed2a2c9a3af777e730c4a86264fe96b7c
feerchsanchez <78006285+feerchsanchez@users.noreply.github.com>
2023-11-19 11:45:29 -0800

    Update DrivingSchoolNameSearch.php

--- DrivingSchoolNameSearch.php
+++ DrivingSchoolNameSearch.php
@@ -1,1 +1,14 @@
+<?php
+// Include the database connection file
+require_once('db_connect.php');
 
+$keywordsearch = $_GET("keyword");
+
+//Search database for driving school with name = $keywordsearch
+
+echo <h2> "Showing records with Driver School name $keywordsearch " </h2>;
+
+$sql = "SELECT DSName, CertRegNumber, Manager FROM DrivingSchool WHERE DSName LIKE '%". $keywordsearch ."%' ";
+$result 
+
+?>
