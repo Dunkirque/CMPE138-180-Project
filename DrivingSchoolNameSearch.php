9fee3447cbe6e98892341a5b8d79d146cf123bf4
feerchsanchez <78006285+feerchsanchez@users.noreply.github.com>
2023-11-19 11:50:35 -0800

    Update DrivingSchoolNameSearch.php

--- DrivingSchoolNameSearch.php
+++ DrivingSchoolNameSearch.php
@@ -11,6 +11,17 @@ echo <h2> "Showing records with Driver School name $keywordsearch " </h2>;
 $sql = "SELECT DSName, CertRegNumber, Manager FROM DrivingSchool WHERE DSName LIKE '%". $keywordsearch ."%' ";
 $result = $mysqli->query($sql);
 
+if ($result->num_rows >0) {
+
+  //output data of each row
+  while ($row = $result->fetch_assoc() )
+    {
+      echo "Driving School: {$row['DSName']} - Cert Reg Number: {$row['CertRegNumber']}, Manager: {$row['Manager']}";
+
+    }
+} else {
+  echo "No results matched the search";
+}
 
 
 ?>
